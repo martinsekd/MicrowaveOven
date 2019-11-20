2e393ef7b78aa0928e98066161ab201532b3e834
Marcus Bech <43644925+morogfar21@users.noreply.github.com>
2019-11-20 14:50:54 +0100

    Added references to microwaveovenClasses

--- Microwave.test.integration/Step3.cs
+++ Microwave.test.integration/Step3.cs
@@ -5,13 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using NSubstitute;
 using NUnit.Framework;
+using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Boundary;
 
 namespace Microwave.test.integration
 {
     [TestFixture]
     public class Step3
     {
-        
+        private Output _output;
+        private Display _display;
+        private PowerTube _powerTube;
+        private ITimer _timer;
 
         [SetUp]
         public void SetUp()
